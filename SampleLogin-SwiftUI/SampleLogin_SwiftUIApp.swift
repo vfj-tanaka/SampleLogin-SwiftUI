a58ff5a28d89a877c13b3228ad8f21968ce50285
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-10-31 11:00:19 +0900

    Initial Commit

---
+++ SampleLogin-SwiftUI/SampleLogin_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLogin_SwiftUIApp.swift
+//  SampleLogin-SwiftUI
+//
+//  Created by mtanaka on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct SampleLogin_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
